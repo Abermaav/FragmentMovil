a169fee702a0af3bbefb23461ed548a9631393b3
Abermaav <abrilerandima@gmail.com>
2025-03-31 14:34:01 -0600

    fase2onose

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        var viewPager = findViewById(R.id.viewPager) as ViewPager
+        var tabLayout = findViewById(R.id.tabLayout) as TabLayout
+
+        val fragmentAdapter = FragmentAdapter(supportFragmentManager) //7poner y agregar yk
+        fragmentAdapter.addFragment(BackFragment(), "Backend")
+        fragmentAdapter.addFragment(FrontFragment(), "Frontend")
+        fragmentAdapter.addFragment(FullFragment(), "FullStack")
+
+        viewPager.adapter = fragmentAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.back)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.front)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.full)
+
+
     }
 }
